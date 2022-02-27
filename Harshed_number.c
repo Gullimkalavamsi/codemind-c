a936ef909213771a50e2bcfc9c0514b31676a8f9
Gullimkalavamsi <97437472+Gullimkalavamsi@users.noreply.github.com>
2022-02-27 15:48:57 +0530

    #include<stdio.h> int main() {     int n,r,i,sum=0,count=0;     scanf("%d",&n);     i=n;     while(n!=0)     {         r=n%10;         sum=sum+r;         n=n/10;     }     if(i%sum==0)     {         printf("True");     }     else     {         printf("False");     }     return 0; }

---
+++ Harshed_number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,i,sum=0,count=0;
+    scanf("%d",&n);
+    i=n;
+    while(n!=0)
+    {
+        r=n%10;
+        sum=sum+r;
+        n=n/10;
+    }
+    if(i%sum==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+    return 0;
+}
