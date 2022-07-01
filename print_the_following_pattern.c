93a072085c4375103a2aaeaed7ba7317bddb8b3e
Gullimkalavamsi <97437472+Gullimkalavamsi@users.noreply.github.com>
2022-07-01 10:03:06 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=0;i<n;i++)     {         for(int j=0;j<n;j++)         {             if(i==j or i-j==n+1)             {                 printf("0");             }             else             {                 printf("x");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -7,13 +7,13 @@ int main()
     {
         for(int j=0;j<n;j++)
         {
-            if(i==j || i+j==n-1)
+            if(i==j or i-j==n+1)
             {
-                printf("x");
+                printf("0");
             }
             else
             {
-                printf("0");
+                printf("x");
             }
         }
         printf("
